d286336547387552e47a0e7529b9bafa85670478
艾晓磊 <1178013314@qq.com>
2020-09-23 15:23:54 +0800

    基本集成结束

---
+++ README.md
@@ -0,0 +1,1 @@
+vue3+electroe+vite+vuerouter
--- src/render/main.js
+++ src/render/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import routers from './routers'
 
-window?window.require = function(){
-  return {}
-}:null
 
 createApp(App).use(routers). mount('#app')
